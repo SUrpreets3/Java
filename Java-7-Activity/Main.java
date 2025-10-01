aa5b86cad9c14cd6632a19f343c64d3298f0f81b
SUrpreets3 <SUrpreets3@nycstudents.net>
2025-10-01 16:29:03 +0000

    work

---
+++ Java-7-Activity/Main.java
@@ -0,0 +1,48 @@
+  
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+/* For each of the challenges below:
+    a) declare the variables, 
+    b) ask the user for the variable values, 
+    c) write the specified equation, and 
+    d) display the equation value.
+*/    
+ 
+//    Challenge 1:  See file Actvy7_Eq1  (done for you)
+      System.out.println("Enter the value for x ");
+      double x = Input.readDouble();
+      double y = Math.pow(x,7);
+      System.out.println("Value of y is "+y);   
+  
+//    Challenge 2:  See file Actvy7_Eq2
+      System.out.println("Enter the value for c ");
+      double a = Input.readDouble();
+      System.out.println("Enter the value for b ");
+      double b = Input.readDouble();
+      double c = Math.sqrt(a+b);
+      System.out.println("the answer to Eq2 is " c ");
+//    Challenge 3:  See file Actvy7_Eq3
+    
+  
+//    Challenge 4:  See file Actvy7_Eq4
+  
+
+//    Challenge 5:  See file Actvy7_Eq5
+    
+  
+//    Challenge 6:  See file Actvy7_Eq6
+
+
+//    Challenge 7:  See file Actvy7_Eq7    (HINT: What does the "plus minus: after "-b" mean?)
+
+    
+
+    // **************************************************
+    // ******** Don't write any code below here. ********
+    // **************************************************
+  }
+}
